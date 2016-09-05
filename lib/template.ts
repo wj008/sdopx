1997dcc179d57e229b83baa629b100b6e75afdc9
sdopx <26029682@qq.com>
2016-09-05 22:30:10 +0800

    Add files via upload

--- lib/template.ts
+++ lib/template.ts
@@ -181,7 +181,7 @@ export class Template {
         }
         catch (e) {
             if (e instanceof SyntaxError) {
-                e.message += ' while compiling ejs';
+                e.message += ' while compiling sdp';
             }
             throw e;
         }
