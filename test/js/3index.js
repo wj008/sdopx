dc92bb157bfdb44c11ba372542f8936b8febc2da
wj008 <26029682@qq.com>
2016-09-27 01:31:19 +0800

    更新 帮助 3 所需的测试

---
+++ test/js/3index.js
@@ -0,0 +1,14 @@
+//模板变量赋值测试例子
+var path = require('path');
+var Sdopx = require("../../sdopx").Sdopx;
+//Sdopx.create_runfile = true; //输出编译后的文件
+
+//设置模板路径
+Sdopx.view_paths = path.join(__dirname, '../views/');
+
+var opx = new Sdopx();
+opx.assign('title', 'hello sdopx');
+opx.assign('foot_content', 'All rights reserved.');
+opx.assign('meetingPlace', 'New York');
+var outhtml = opx.display('3index');
+console.log(outhtml);
