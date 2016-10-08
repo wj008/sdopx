961ae3e1a17928d38ad961f7a2f0a5b83f73c0d6
wj008 <26029682@qq.com>
2016-10-09 02:57:21 +0800

    添加 option 过滤器的示例

--- test/js/modefier_option.js
+++ test/js/modefier_option.js
@@ -1,7 +1,7 @@
 //测试分界符号指定 可使用命令行直接运行
 var path = require('path');
 var Sdopx = require("../../sdopx").Sdopx;
-Sdopx.create_runfile = true; //输出编译后的文件
+//Sdopx.create_runfile = true; //输出编译后的文件
 
 //设置模板路径
 Sdopx.view_paths = path.join(__dirname, '../views/');
@@ -11,6 +11,7 @@ var opx = new Sdopx();
 //注册变量 简单的变量 (非数组/对象)
 opx.assign('bar', 'bar');
 opx.assign('name', 'name');
+opx.assign('bool', true);
 opx.assign('option', {bar: 'option1_bar', name: 'option_name'});
 var outhtml = opx.display('modefier_option');
 console.log(outhtml);
