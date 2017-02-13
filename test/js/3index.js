f241c58ee53d199fe5e475cffa0d711d778946c4
wj008 <26029682@qq.com>
2017-02-14 01:47:24 +0800

    调整测试 在循环中插入代码 或使用 block块的bug调试

--- test/js/3index.js
+++ test/js/3index.js
@@ -1,7 +1,7 @@
 //模板变量赋值测试例子
 var path = require('path');
 var Sdopx = require("../../sdopx").Sdopx;
-//Sdopx.create_runfile = true; //输出编译后的文件
+Sdopx.create_runfile = true; //输出编译后的文件
 
 //设置模板路径
 Sdopx.view_paths = path.join(__dirname, '../views/');
@@ -10,5 +10,6 @@ var opx = new Sdopx();
 opx.assign('title', 'hello sdopx');
 opx.assign('foot_content', 'All rights reserved.');
 opx.assign('meetingPlace', 'New York');
+opx.assign('list', [{id: 1, name: 'aaa'}, {id: 2, name: 'bbb'}]);
 var outhtml = opx.display('3index');
 console.log(outhtml);
