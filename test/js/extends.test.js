08c3698c09423fb4d22cb015f8fd00b387773a0a
wj008 <26029682@qq.com>
2017-03-06 12:57:39 +0800

    使用 display 指定母版输出案例

---
+++ test/js/extends.test.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Administrator on 2017/3/6.
+ */
+//模板变量赋值测试例子
+var path = require('path');
+var Sdopx = require("../../sdopx").Sdopx;
+//Sdopx.create_runfile = true; //输出编译后的文件
+
+//设置模板路径
+Sdopx.view_paths = path.join(__dirname, '../views/');
+
+var opx = new Sdopx();
+opx.assign('title', 'hello sdopx');
+opx.assign('foot_content', 'All rights reserved.');
+opx.assign('meetingPlace', 'New York');
+opx.assign('list', [{id: 1, name: 'aaa'}, {id: 2, name: 'bbb'}]);
+var outhtml = opx.display('extends:4layout|3index');
+console.log(outhtml);
